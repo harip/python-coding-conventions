363378632d713ec5d8a8d2cee8967ebebe1b93b8
Hari Pulugurta <charanp@gmail.com>
2018-05-12 01:23:09 -0400

    wip - bug fix for double operators

--- src/postion.ts
+++ src/postion.ts
@@ -1,9 +1,25 @@
+import * as opr from './operators'; 
+
 export const getAllOccurences=(operator:string,line:string)=>{
     let operatorLocations=[];
     let idx=line.indexOf(operator);
     while (idx!==-1){
-        operatorLocations.push(idx);
-        idx = line.indexOf(operator, idx + 1);
+        // Get the next element
+        let nextElem=line.substr(idx,1);
+        let isMultiOperator=opr.doubleOperators.findIndex((e)=>{
+            return e===nextElem;
+        })
+
+        if (isMultiOperator!==-1){
+            // Its like ==
+            // Skip this
+            operatorLocations.push(idx);
+            idx = line.indexOf(operator, idx + 2);
+        }   
+        else{
+            operatorLocations.push(idx);
+            idx = line.indexOf(operator, idx + 1);
+        } 
     }
     return operatorLocations;
 };
