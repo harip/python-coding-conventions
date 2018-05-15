321f7d0f460462c06b84d4acfb34783fe280f29d
Hari Pulugurta <charanp@gmail.com>
2018-05-15 12:07:07 -0400

    added basic test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -4,10 +4,14 @@ import {LineInfo} from '../linedata';
 import * as conv from '../conventions';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Test Spaces Around Equal To", function () {
-    let lineText="x=y";
-    let lineInfo=new LineInfo(lineText,0);
+suite("Coding convetions Tests",()=>{
 
-    let r=conv.getConventions(lineInfo);
-    assert.equal(r[0].Operator," = ",`The resulting text should be x = y, but was returned as ${r[0].Operator}`);
+    test("Test Spaces around equals to operator",()=>{
+        let lineText="x=y";
+        let lineInfo=new LineInfo(lineText,0);
+    
+        let r=conv.getConventions(lineInfo);
+        assert.equal(r[0].Operator," = ",`The resulting text should be x = y, but was returned as ${r[0].Operator}`);
+    });
+
 });
