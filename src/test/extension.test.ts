8e627bf6a8eee09bf1dfc4f2d21be8c30db677a6
Hari Pulugurta <charanp@gmail.com>
2018-05-15 01:51:43 -0400

    Added a basic test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,22 +1,13 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+import {LineInfo} from '../linedata';
+import * as conv from '../conventions';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function () {
+suite("Test Spaces Around Equal To", function () {
+    let lineText="x=y";
+    let lineInfo=new LineInfo(lineText,0);
 
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
+    let r=conv.getConventions(lineInfo);
+    assert.equal(r[0].Operator," = ",`The resulting text should be x = y, but was returned as ${r[0].Operator}`);
 });
